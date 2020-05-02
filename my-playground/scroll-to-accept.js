0ff01090bd5ed89e93280823ff39c77a35a9e855
Jonathan Ramos <tan.ramos.web@gmail.com>
2020-05-02 18:15:54 +0100

    scroll to accept, if statements

--- my-playground/scroll-to-accept.js
+++ my-playground/scroll-to-accept.js
@@ -16,17 +16,19 @@
 // }
 // scrollToAccept();
 
-console.log('Scroll events works!!!!');
+console.log('Scroll events works!!!!!');
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
 
 function obCallback(payload) {
   console.log(payload);
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    ob.unobserve(terms.lastElementChild);
+  }
 }
-const ob = new IntersectionObserver(obCallback);
 
-terms.addEventListener('scroll', function(e) {
-  // console.log(e);
-  console.log(e.currentTarget.scrollTop);
-  console.log(e.currentTarget.scrollHeight);
-});
+const ob = new IntersectionObserver(obCallback, { root: terms, threshold: 1 });
+
+ob.observe(terms.lastElementChild);
